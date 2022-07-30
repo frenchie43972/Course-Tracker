3facec6d665170393fbddd007e26218052333209
Kris French <frenchie43972@gmail.com>
2022-07-31 07:48:31 +0900

    Just started

---
+++ CourseTracker/CourseTracker/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CourseTracker
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+			var homePage = new HomePage();
+			var navPage = new NavigationPage(homePage);
+			MainPage = navPage;
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
